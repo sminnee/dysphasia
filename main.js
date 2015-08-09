b576a2930052bc6bcf8213b608fe56adee860b19
Sam Minnee <sam@silverstripe.com>
2015-08-09 20:54:36 +1200

    Improve parse error output; added debug info.

--- main.js
+++ main.js
@@ -10,17 +10,34 @@ var inputFile = userArgs[0];
 
 Dysphasia.recompileParserIfNeeded(fs, './src/parser', function() {
   fs.readFile(inputFile, function (err, data) {
-    var dp = Dysphasia.loadString(data.toString());
+    var code = data.toString();
     // process.stdout.write(JSON.stringify(dp.parseTree(), null, '  ') + '\n');
     // process.stdout.write(dp.generateLLVMCode());
 
     var llFile = inputFile.replace(/\.dp$/, '.ll');
     var outFile = inputFile.replace(/\.dp$/, '');
 
-    fs.writeFileSync(llFile, dp.generateLLVMCode());
+    try {
+      var dp = Dysphasia.loadString(code);
+      var compiled = dp.generateLLVMCode();
+    } catch(e) {
+      if(e.offset) {
+        console.error('Parse error on line ' + e.line + ':\n' + e.message + '\n...\n' 
+          + code.substr(e.offset).match(/^.*\n(.*\n)?(.*\n)?(.*\n)?/)[0]
+          + '...\n');
+        process.exit(1);
+      } else {
+        throw(e);
+      }
+    }
+
+    console.log('Compiled code:\n' + compiled + '\n');
+
+    fs.writeFileSync(llFile, compiled);
 
     exec(LLVM_CONFIG + ' --cxxflags --ldflags --system-libs --libs core', function (err, stdout, stderr) {
       var flags = stdout.replace(/\n/g, ' ');
+      console.log('Calling: ' + CLANG + ' ' + llFile + ' ' + flags + ' -o ' + outFile + '\n');
       exec(CLANG + ' ' + llFile + ' ' + flags + ' -o ' + outFile, function (err, stdout, stderr) {
         if(err) {
           console.error(stderr);
