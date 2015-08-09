7d23a0491a67f51bd184cd47029cfeadda36b69b
Sam Minnee <sam@silverstripe.com>
2015-08-09 19:21:48 +1200

    Code style tidy-up.

--- src/dysphasia.js
+++ src/dysphasia.js
@@ -16,15 +16,15 @@ DysphasiaFile.prototype.generateLLVMCode = function () {
 
 
 var Dysphasia = {
-  loadString: function(string) {
+  loadString: function (string) {
     return new DysphasiaFile(string);
   },
 
   recompileParser: function (fs, inFile, outFile, callback) {
-    fs.readFile(inFile, function(err, data) {
-      var PEG = require("pegjs");
+    fs.readFile(inFile, function (err, data) {
+      var PEG = require('pegjs');
 
-      console.warn("Recompiling parser...");
+      console.warn('Recompiling parser...');
       var parserSource = PEG.buildParser(data.toString(), { output: 'source' });
       fs.writeFile(outFile, 'module.exports = ' + parserSource + ';\n', callback);
     });
@@ -34,17 +34,16 @@ var Dysphasia = {
     var inFile = basePath + '/parser.pegjs';
     var outFile = basePath + '/parser.js';
 
-    fs.stat(outFile, function(err, outStat) {
-      fs.stat(inFile, function(err, inStat) {
-        if(Date.parse(outStat.mtime) < Date.parse(inStat.mtime)) {
+    fs.stat(outFile, function (err, outStat) {
+      fs.stat(inFile, function (err, inStat) {
+        if (Date.parse(outStat.mtime) < Date.parse(inStat.mtime)) {
           Dysphasia.recompileParser(fs, inFile, outFile, callback);
-
         } else {
           callback();
         }
       });
     });
   }
-}
+};
 
 module.exports = Dysphasia;
