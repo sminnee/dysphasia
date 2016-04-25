ad9169df95ab6bb6924c78cc55227ea635134ec3
Sam Minnee <sam@silverstripe.com>
2016-04-25 16:24:22 +1200

    MINOR: Tidied up test layout.

--- test/dysphasia.js
+++ test/dysphasia.js
@@ -7,67 +7,85 @@ var fs = require('fs');
 var InlineFnGuards = require('../src/ast-transforms/InlineFnGuards');
 var InferTypes = require('../src/ast-transforms/InferTypes');
 
-describe('Dysphasia', function () {
-  it('supports if statements', function () {
-    testIntermediateCode('test/if-else.dptest');
-  });
-  it('supports basic arithmetic', function () {
-    testASTInferredTypes('test/arithmetic.dptest');
-    testIntermediateCode('test/arithmetic.dptest');
-  });
-  it('supports variable assignment', function () {
-    testAST('test/variable-assignment.dptest');
-  });
-  it('supports type inferencing in variable assignment', function () {
-    testASTTransform('test/variable-assignment.dptest', [new InferTypes()]);
-  });
-  it('variable assignments can be compiled', function () {
-    testIntermediateCode('test/variable-assignment.dptest');
-  });
-  it('supports comparison operator', function () {
-    testAST('test/comparison.dptest');
-    testIntermediateCode('test/comparison.dptest');
-  });
-  it('supports use and puts', function () {
-    testASTTransform('test/use-puts.dptest', [new InferTypes()]);
-    testIntermediateCode('test/use-puts.dptest');
-  });
-  it('supports for loops without a bound variable', function () {
-    testIntermediateCode('test/for-loop.dptest');
-  });
-  it('supports for loops with a bound variable', function () {
-    testAST('test/for-loop-with-variable.dptest');
-    testIntermediateCode('test/for-loop-with-variable.dptest');
-  });
-  it('supports for loops iterating an array', function () {
-    testASTTransform('test/for-loop-array.dptest', [new InferTypes()]);
-    testIntermediateCode('test/for-loop-array.dptest');
-  });
-  it('supports comments', function () {
+describe('The Dysphasia language', function () {
+  it('supports comments (comments)', function () {
     testAST('test/comments.dptest');
   });
-  it('can convert int to string', function () {
-    testIntermediateCode('test/string-casting.dptest');
-  });
-  it('will convert int to string regardless of where the int is', function () {
-    testAST('test/string-casting-variants.dptest');
-  });
-  it('can infer types', function () {
+  it('can infer types (infer types)', function () {
     testASTInferredTypes('test/infer-types.dptest');
   });
-  it('allows calls to user-defined functions', function () {
-    testAST('test/call-custom-fn.dptest');
-    testIntermediateCode('test/call-custom-fn.dptest');
-  });
-  it('allows concise arrow syntax for function definition', function () {
-    testAST('test/concise-functions.dptest');
-    testASTTransform('test/concise-functions.dptest', [new InlineFnGuards()]);
-  });
-  it('infers ambiguous function types by creating multiple functions', function () {
-    testASTTransform('test/infer-ambiguous-types.dptest', [new InferTypes()]);
-  });
-  it('supports floats and ints', function () {
+  it('supports floats and ints (floats-and-ints)', function () {
     testAST('test/floats-and-ints.dptest');
+  });
+
+  describe('Expressions', function () {
+    it('support basic arithmetic (arithmetic)', function () {
+      testASTInferredTypes('test/arithmetic.dptest');
+      testIntermediateCode('test/arithmetic.dptest');
+    });
+    it('support comparison operators (comparison)', function () {
+      testAST('test/comparison.dptest');
+      testIntermediateCode('test/comparison.dptest');
+    });
+    it('supports use and puts', function () {
+      testASTTransform('test/use-puts.dptest', [new InferTypes()]);
+      testIntermediateCode('test/use-puts.dptest');
+    });
+  });
+
+  describe('Variable assignments', function () {
+    it('are supported (variable-assignment)', function () {
+      testAST('test/variable-assignment.dptest');
+    });
+    it('supports type inferencing (variable-assignment)', function () {
+      testASTTransform('test/variable-assignment.dptest', [new InferTypes()]);
+    });
+    it('can be compiled', function () {
+      testIntermediateCode('test/variable-assignment.dptest');
+    });
+  });
+
+  describe('String parsing', function () {
+    it('can convert int to string (string-casting)', function () {
+      testIntermediateCode('test/string-casting.dptest');
+    });
+    it('will convert int to string regardless of where the int is (string-casting-variants)', function () {
+      testAST('test/string-casting-variants.dptest');
+    });
+  });
+
+  describe('For loops', function () {
+    it('work without a bound variable (for-loop)', function () {
+      testIntermediateCode('test/for-loop.dptest');
+    });
+    it('work with a bound variable (for-loop-with-variable)', function () {
+      testAST('test/for-loop-with-variable.dptest');
+      testIntermediateCode('test/for-loop-with-variable.dptest');
+    });
+    it('support iterating an array (for-loop-array)', function () {
+      testASTTransform('test/for-loop-array.dptest', [new InferTypes()]);
+      testIntermediateCode('test/for-loop-array.dptest');
+    });
+  });
+
+  describe('If statements', function () {
+    it('are supports (if-else)', function () {
+      testIntermediateCode('test/if-else.dptest');
+    });
+  });
+
+  describe('Functions', function () {
+    it('can be user-defined (call-custom-fn)', function () {
+      testAST('test/call-custom-fn.dptest');
+      testIntermediateCode('test/call-custom-fn.dptest');
+    });
+    it('can be defined with concise arrow syntax (concise-functions)', function () {
+      testAST('test/concise-functions.dptest');
+      testASTTransform('test/concise-functions.dptest', [new InlineFnGuards()]);
+    });
+    it('infers ambiguous function types by creating multiple functions (infer-ambiguous-types)', function () {
+      testASTTransform('test/infer-ambiguous-types.dptest', [new InferTypes()]);
+    });
   });
 });
 
